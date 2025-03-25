8ab27eda5038375d7de9eff4ad4ae4fe4ed95f3a
ddangmul <j.ddangmul@gmail.com>
2025-03-25 12:50:38 +0900

    search page 수정

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -24,8 +24,15 @@ const SearchPage = () => {
   }, [searchTerm]);
 
   return (
-    <section className="searchPage">
-      <h1>검색 결과</h1>
+    <section className="searchPage py-10 px-20">
+      <div className="cart-heading flex gap-3 items-center">
+        <span className="text-3xl py-10">
+          <span className="font-serif">'{searchTerm}'</span> 관련 검색 결과
+        </span>
+        <span className="text-xl bg-[#494643] text-[#f2f0eb] px-2.5 rounded-full">
+          {products.length}
+        </span>
+      </div>
       {products.length === 0 ? (
         <p>검색어와 일치하는 제품이 없습니다.</p>
       ) : (
