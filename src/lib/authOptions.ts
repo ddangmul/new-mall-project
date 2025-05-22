c12abb2c6a80342540e220fb1b64e500e9575cda
ddangmul <j.ddangmul@gmail.com>
2025-05-22 14:38:11 +0900

    localhost 구글로그인 환경변수명 수정

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -67,8 +67,8 @@ export const authOptions: NextAuthOptions = {
     }),
 
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.NEXT_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
           prompt: "select_account",
